bea1743d80f9d7c5612fba3962e1768b6378fda5
Takaki Ueno <ueknot@gmail.com>
2021-02-13 02:17:27 +0900

    7-6

--- ch07/src/main.rs
+++ ch07/src/main.rs
@@ -1,28 +1,28 @@
-use std::ops::Drop;
+//use std::ops::Drop;
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 struct Parent(usize, Child, Child);
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 struct Child(usize);
 
-impl Drop for Parent {
-    fn drop(&mut self) {
-        println!("Dropping {:?}", self);
-    }
-}
+// impl Drop for Parent {
+//     fn drop(&mut self) {
+//         println!("Dropping {:?}", self);
+//     }
+// }
 
-impl Drop for Child {
-    fn drop(&mut self) {
-        println!("Dropping {:?}", self);
-    }
-}
+// impl Drop for Child {
+//     fn drop(&mut self) {
+//         println!("Dropping {:?}", self);
+//     }
+// }
 
 fn main() {
     let p1 = Parent(1, Child(11), Child(12));
     let p2 = p1;
     println!("p2: {:?}", p2);
-    // println!("p1: {:?}", p1); // this causes error
+    println!("p1: {:?}", p1); // this causes error
 
     let p1 = Parent(2, Child(21), Child(22));
     println!("p1: {:?}", p1);
