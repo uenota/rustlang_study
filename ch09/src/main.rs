b6c352909af6d03187fe1c170fc94d8238630d6d
Takaki Ueno <ueknot@gmail.com>
2021-03-01 04:04:02 +0900

    Add code example for Into trait in chap 8

--- ch09/src/main.rs
+++ ch09/src/main.rs
@@ -1,3 +1,12 @@
+fn take_string(s: impl Into<String>) {
+	let _s = s.into();
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 文字列リテラル (str)
+    take_string("str");
+
+    // String
+    let arg = "str".to_string();
+    take_string(arg);
 }
